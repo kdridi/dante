1c87631da82b3c1ad57a62aaf001134de77f016b
Karim DRIDI <karim.dridi@gmail.com>
2020-03-10 20:46:56 +0100

    add .travis.yml

---
+++ index.js
@@ -0,0 +1,11 @@
+const API = require('./data/api')
+
+const main = async (api) => {
+	const { students, slug, workdir } = api
+	return JSON.stringify({ students, slug, workdir }, null, 2)
+}
+
+main(new API()).then(
+	(res) => console.log(res),
+	(err) => console.error(err)
+)
