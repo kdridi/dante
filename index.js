0e07d4b18e195c4caaba38fcaf0baeaeb8362d8d
Karim DRIDI <karim.dridi@gmail.com>
2020-03-10 22:52:01 +0100

    add .travis.yml

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
+const ProgressBar = require('progress')
 const execa = require('execa')
 const path = require('path')
 const API = require('./data/api')
 
 const main = async (api) => {
 	const requests = await api.getRepositoryList()
-	const responses = []
+	const bar = new ProgressBar('downloading [:bar] :percent :etas :current/:total  ', { total: requests.length })
 
-	for (let index = 0; index < requests.length; index++) {
-		const request = requests[index]
+	const responses = await Promise.all(
+		requests.map(async (request) => {
+			const { url, wdir, tdir } = request
 
-		console.error(request)
-		const { url, wdir, tdir } = request
-
-		{
-			console.error('git clone', tdir)
-			await execa('git', ['clone', tdir, path.resolve(wdir, 'tests')])
-			console.error('make -C', path.resolve(wdir, 'tests'))
-			await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C tests'])
-		}
-
-		let clone = null
-		{
-			console.error('git clone', url)
-			try {
-				clone = await execa('git', ['clone', url, path.resolve(wdir, 'delivery')])
-			} catch (error) {
-				clone = error
+			{
+				await execa('git', ['clone', tdir, path.resolve(wdir, 'tests')])
+				await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C tests'])
 			}
-		}
 
-		let build = null
-		{
-			console.error('make -C', path.resolve(wdir, 'delivery'))
-			try {
-				build = await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C delivery'])
-			} catch (error) {
-				build = error
+			let clone = null
+			{
+				try {
+					clone = await execa('git', ['clone', url, path.resolve(wdir, 'delivery')])
+				} catch (error) {
+					clone = error
+				}
 			}
-		}
 
-		const steps = { clone, build }
-		console.error(request, steps)
+			let build = null
+			{
+				try {
+					build = await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C delivery'])
+				} catch (error) {
+					build = error
+				}
+			}
 
-		responses.push(Object.assign({ steps }, request))
-	}
+			const steps = { clone, build }
+			bar.tick()
 
+			return Object.assign(request, { steps })
+		})
+	)
 	return JSON.stringify(responses, null, 2)
 }
 
