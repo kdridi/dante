04727c09f5afda54abee8054f60cc3c5b7bea58e
Karim DRIDI <karim.dridi@gmail.com>
2020-03-10 22:24:46 +0100

    add .travis.yml

--- index.js
+++ index.js
@@ -4,40 +4,43 @@ const API = require('./data/api')
 
 const main = async (api) => {
 	const requests = await api.getRepositoryList()
-	const responses = await Promise.all(
-		requests.map(async (request) => {
-			console.error(request)
-			const { url, wdir, tdir } = request
+	const responses = []
 
-			{
-				await execa('git', ['clone', tdir, path.resolve(wdir, 'tests')])
-				await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C tests'])
+	for (let index = 0; index < requests.length; index++) {
+		const request = requests[index]
+
+		console.error(request)
+		const { url, wdir, tdir } = request
+
+		{
+			await execa('git', ['clone', tdir, path.resolve(wdir, 'tests')])
+			await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C tests'])
+		}
+
+		let clone = null
+		{
+			try {
+				clone = await execa('git', ['clone', url, path.resolve(wdir, 'delivery')])
+			} catch (error) {
+				clone = error
 			}
+		}
 
-			let clone = null
-			{
-				try {
-					clone = await execa('git', ['clone', url, path.resolve(wdir, 'delivery')])
-				} catch (error) {
-					clone = error
-				}
+		let build = null
+		{
+			try {
+				build = await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C delivery'])
+			} catch (error) {
+				build = error
 			}
+		}
 
-			let build = null
-			{
-				try {
-					build = await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C delivery'])
-				} catch (error) {
-					build = error
-				}
-			}
+		const steps = { clone, build }
+		console.error(request, steps)
 
-			const steps = { clone, build }
-			console.error(request, steps)
+		responses.push(Object.assign({ steps }, request))
+	}
 
-			return Object.assign(request, { steps })
-		})
-	)
 	return JSON.stringify(responses, null, 2)
 }
 
