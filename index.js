e5ab09704c01f6a36a59e6a68278966ce514ec3d
Karim DRIDI <karim.dridi@gmail.com>
2020-03-11 15:25:10 +0100

    add .travis.yml

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const main = async (api) => {
 
 	const responses = await Promise.all(
 		requests.map(async (request) => {
-			const { wdir, deliveryURL, testsURL } = request
+			const { wdir, deliveryURL, testsURL, artifacts } = request
 
 			const tests = { clone: null, make: null }
 			tests.clone = await api.gitClone(wdir, testsURL, 'tests')
@@ -19,7 +19,25 @@ const main = async (api) => {
 			delivery.clone = await api.gitClone(wdir, deliveryURL, 'delivery')
 			delivery.make = await api.dockerRun(wdir, 'make', '-C', 'delivery')
 
-			const steps = { tests, delivery }
+			const deploy = (
+				await Promise.all(
+					artifacts.map(async (artifact) => {
+						const dirname = artifact.split('/')
+						const basename = dirname.pop()
+
+						const tpath = ['tests', ...dirname].join('/')
+						const fpath = ['delivery', ...dirname, basename].join('/')
+
+						const data = { mkdir: null, copy: null }
+						data.mkdir = await api.dockerRun(wdir, 'mkdir', '-p', tpath)
+						data.copy = await api.dockerRun(wdir, 'cp', fpath, tpath)
+
+						return { artifact, data }
+					})
+				)
+			).reduce((res, { artifact, data }) => Object.assign(res, { [artifact]: data }), {})
+
+			const steps = { tests, delivery, deploy }
 			bar.tick()
 
 			return Object.assign(request, { steps })
