5f557bc8061c46bee5c722e8650f832b7d061143
Karim DRIDI <karim.dridi@gmail.com>
2020-03-11 10:01:49 +0100

    add .travis.yml

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
 const ProgressBar = require('progress')
-const execa = require('execa')
-const path = require('path')
 const API = require('./data/api')
 
 const main = async (api) => {
 	const requests = await api.getRepositoryList()
+	console.log(requests)
+
 	const bar = new ProgressBar('downloading [:bar] :percent :etas :current/:total  ', { total: requests.length })
 
 	const responses = await Promise.all(
 		requests.map(async (request) => {
-			const { url, wdir, tdir } = request
+			const { wdir, deliveryURL, testsURL } = request
 
 			{
-				await execa('git', ['clone', tdir, path.resolve(wdir, 'tests')])
-				await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C tests'])
+				api.gitClone(wdir, testsURL, 'tests')
+				api.dockerRun(wdir, 'make', '-C', 'tests')
 			}
 
-			let clone = null
-			{
-				try {
-					clone = await execa('git', ['clone', url, path.resolve(wdir, 'delivery')])
-				} catch (error) {
-					clone = error
-				}
-			}
-
-			let build = null
-			{
-				try {
-					build = await execa('docker', ['container', 'run', '--rm', '--network', 'none', '-v', `${wdir}:/app`, '-w', '/app', 'epitechcontent/epitest-docker', 'bash', '-c', 'make -C delivery'])
-				} catch (error) {
-					build = error
-				}
-			}
+			const clone = api.gitClone(wdir, deliveryURL, 'delivery')
+			const build = api.dockerRun(wdir, 'make', '-C', 'delivery')
 
 			const steps = { clone, build }
 			bar.tick()
